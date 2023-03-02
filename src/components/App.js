400f135d1f0562454e47f3d15037b562db920137
jos-the-top-dev <devs.josia@gmail.com>
2023-03-03 01:05:39 +0300

    Reverting to 70811f2f1bb75ad537fcd875fb5af1779dfc22e3

--- src/components/App.js
+++ src/components/App.js
@@ -1,48 +1,8 @@
-import React from 'react';
-import { Container, } from 'semantic-ui-react';
 import '../App.css';
-import Header from './Header';
-import Home from './Home';
-import About from './About';
-import Services from './Services';
-import Faqs from './Faqs';
-import SignUp from './Signup';
-import Footer from './Footer';
 
 function App() {
   return (
-    <div className='my-app'>
-    <Header/>
-
-    <Home/>
-
-    <Container text style={{ marginTop: '17em' }}>
-      <About/>
-    </Container>
-
-    <Container text style={{ marginTop: '17em' }}>
-      <Services/>
-    </Container>
-
-    <Container text style={{ marginTop: '17em' }}>
-      <Faqs/>
-    </Container>
-
-    <Container text style={{ marginTop: '17em' }}>
-      <SignUp/>
-    </Container>
-
-    
-    
-
-    <Container text style={{ marginTop: '17em' }}>
-      <Footer/>
-    </Container>
-
-
-
-
-    </div>
+    <div>Hello World</div>
   );
 }
 
