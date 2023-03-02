13ba2306cd132dd9ed40143da35ad0af8977919f
jos-the-top-dev <devs.josia@gmail.com>
2023-03-03 02:48:42 +0300

    formatting with prettier

--- src/components/Signup.js
+++ src/components/Signup.js
@@ -1,17 +1,17 @@
-import React, { useState } from 'react';
-import { Container, Form, Grid, Segment, Dropdown } from 'semantic-ui-react';
-import '../App.css';
+import React, { useState } from "react";
+import { Container, Form, Grid, Segment, Dropdown } from "semantic-ui-react";
+import "../App.css";
 
 const Signup = () => {
   const [formData, setFormData] = useState({
-    fullName: '',
-    email: '',
-    phoneNumber: '',
-    occupation: '',
-    location: '',
-    accountType: '',
-    nextOfKin: '',
-    nextOfKinPhoneNumber: '',
+    fullName: "",
+    email: "",
+    phoneNumber: "",
+    occupation: "",
+    location: "",
+    accountType: "",
+    nextOfKin: "",
+    nextOfKinPhoneNumber: "",
   });
 
   const handleChange = (event) => {
@@ -23,22 +23,21 @@ const Signup = () => {
       const response = await fetch(endpoint, {
         method: "POST",
         headers: {
-          "Content-Type": "application/json"
+          "Content-Type": "application/json",
         },
-        body: JSON.stringify(formData)
+        body: JSON.stringify(formData),
       });
-  
+
       if (!response.ok) {
         throw new Error("Network response was not ok");
       }
-  
+
       const data = await response.json();
       return data;
     } catch (error) {
       console.error("Error posting data: ", error);
     }
   };
-  
 
   const handleSubmit = (event) => {
     event.preventDefault();
@@ -46,96 +45,108 @@ const Signup = () => {
     const endpoint = "https://localhost:9292/users/";
     const formData = postData(formData, endpoint);
     console.log(formData);
-
-
   };
+  
   const handleDropdownChange = (e, { value }) => {
     setFormData({ ...formData, accountType: value });
   };
 
   const accountOptions = [
-    { key: 'current', text: 'Current', value: 'current' },
-    { key: 'savings', text: 'Savings', value: 'savings' }
+    { key: "current", text: "Current", value: "current" },
+    { key: "savings", text: "Savings", value: "savings" },
   ];
 
   return (
-    <Container id='section4' className='signup' >
-      <h1 className='signup-title' >Sign Up</h1>
+    <Container id="section4" className="signup">
+      <h1 className="signup-title">Sign Up</h1>
       <Segment>
         <Form onSubmit={handleSubmit}>
           <Grid columns={2}>
             <Grid.Column>
               <Form.Field>
                 <label>Full Name</label>
-                <input name="fullName" 
-                onChange={handleChange} 
-                value={formData.fullName} 
-                placeholder="Full Name" 
-                required />
+                <input
+                  name="fullName"
+                  onChange={handleChange}
+                  value={formData.fullName}
+                  placeholder="Full Name"
+                  required
+                />
               </Form.Field>
               <Form.Field>
                 <label>Email</label>
-                <input name="email" 
-                onChange={handleChange} 
-                value={formData.email} 
-                placeholder="Email" 
-                required />
+                <input
+                  name="email"
+                  onChange={handleChange}
+                  value={formData.email}
+                  placeholder="Email"
+                  required
+                />
               </Form.Field>
               <Form.Field>
                 <label>Phone Number</label>
-                <input name="phoneNumber" 
-                onChange={handleChange} 
-                value={formData.phoneNumber} 
-                placeholder="Phone Number" 
-                required/>
+                <input
+                  name="phoneNumber"
+                  onChange={handleChange}
+                  value={formData.phoneNumber}
+                  placeholder="Phone Number"
+                  required
+                />
               </Form.Field>
             </Grid.Column>
             <Grid.Column>
               <Form.Field>
                 <label>Occupation</label>
-                <input name="occupation" 
-                onChange={handleChange} 
-                value={formData.occupation} 
-                placeholder="Occupation" 
-                required/>
+                <input
+                  name="occupation"
+                  onChange={handleChange}
+                  value={formData.occupation}
+                  placeholder="Occupation"
+                  required
+                />
               </Form.Field>
               <Form.Field>
                 <label>Location</label>
-                <input name="location" 
-                onChange={handleChange} 
-                value={formData.location} 
-                placeholder="Location" 
-                required/>
+                <input
+                  name="location"
+                  onChange={handleChange}
+                  value={formData.location}
+                  placeholder="Location"
+                  required
+                />
               </Form.Field>
               <Form.Field>
                 <label>Account Type</label>
                 <Dropdown
-            placeholder="Select account type"
-            fluid
-            selection
-            options={accountOptions}
-            onChange={handleDropdownChange}
-            value={formData.accountType}
-          />
-
+                  placeholder="Select account type"
+                  fluid
+                  selection
+                  options={accountOptions}
+                  onChange={handleDropdownChange}
+                  value={formData.accountType}
+                />
               </Form.Field>
             </Grid.Column>
           </Grid>
           <Form.Field>
             <label>Next of Kin</label>
-            <input name="nextOfKin" 
-            onChange={handleChange} 
-            value={formData.nextOfKin} 
-            placeholder="Next of Kin" 
-            required/>
+            <input
+              name="nextOfKin"
+              onChange={handleChange}
+              value={formData.nextOfKin}
+              placeholder="Next of Kin"
+              required
+            />
           </Form.Field>
           <Form.Field>
             <label>Next of Kin Phone Number</label>
-            <input name="nextOfKinPhoneNumber" 
-            onChange={handleChange} 
-            value={formData.nextOfKinPhoneNumber} 
-            placeholder="Next of Kin Phone Number"
-            required />
+            <input
+              name="nextOfKinPhoneNumber"
+              onChange={handleChange}
+              value={formData.nextOfKinPhoneNumber}
+              placeholder="Next of Kin Phone Number"
+              required
+            />
           </Form.Field>
           <Form.Button primary>Submit</Form.Button>
         </Form>
