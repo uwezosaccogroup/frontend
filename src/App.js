7ba3b061ebc59c4ec56e178dbb9a19c7450207cd
jos-the-top-dev <devs.josia@gmail.com>
2023-03-03 04:12:41 +0300

    dealing with navigation

--- src/App.js
+++ src/App.js
@@ -6,22 +6,22 @@ import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min";
 
 import Landing from "./components/landingPage/Landing";
-import Header from "./components/landingPage/components/Header";
-// import NavBar from "./components/NavBar";
-// import Profile from "./components/Profile";
-// import Deposit from "./components/Deposit";
-// import Withdraw from "./components/Withdraw";
-// import Savings from "./components/Savings";
+import NavBar from "./components/dashboardPage/components/NavBar";
+import Profile from "./components/dashboardPage/components/Profile";
+import Deposit from "./components/dashboardPage/components/Deposit";
+import Withdraw from "./components/dashboardPage/components/Withdraw";
+import Savings from "./components/dashboardPage/components/Savings";
 
 const App = () => (
   <>
-    <Header />
     <BrowserRouter>
       <Routes>
-        <Route exact path="/home" element={<Landing />} />
-        {/* <Route exact path="/deposit" element={<Deposit />} /> */}
-        {/* <Route exact path="/withdraw" element={<Withdraw />} /> */}
-        {/* <Route exact path="/savings" element={<Savings />} /> */}
+        <Route exact path="/" element={<Landing />} />
+        <Route exact path="/dashboard" element={<NavBar />} />
+        <Route exact path="/dashboard/profile" element={<Profile />} />
+        <Route exact path="/dashboard/deposit" element={<Deposit />} />
+        <Route exact path="/dashboard/withdraw" element={<Withdraw />} />
+        <Route exact path="/dashboard/savings" element={<Savings />} />
       </Routes>
     </BrowserRouter>
   </>
