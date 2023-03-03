6587d2a15dd8605f9aee7ce3a4c863b246afcf8e
Marcus <marcus.kariuki@student.moringaschool.com>
2023-03-03 23:14:32 +0300

    edit sign up form

--- src/components/landingPage/components/Signup.js
+++ src/components/landingPage/components/Signup.js
@@ -68,107 +68,102 @@ const Signup = () => {
 
   return (
     <Container id="section4" >
-      <h1 className="signup-title">Sign Up</h1>
-      <Segment>
-        <Form onSubmit={handleSubmit}>
-          <Grid columns={2}>
-            <Grid.Column>
-              <Form.Field>
-                <label>Full Name</label>
-                <input
-                  name="fullName"
-                  onChange={handleChange}
-                  value={formData.fullName}
-                  placeholder="Full Name"
-                  required
-                />
-              </Form.Field>
-              <Form.Field>
-                <label>Email</label>
-                <input
-                  name="email"
-                  onChange={handleChange}
-                  value={formData.email}
-                  placeholder="Email"
-                  required
-                />
-              </Form.Field>
-              <Form.Field>
-                <label>Phone Number</label>
-                <input
-                  name="phoneNumber"
-                  onChange={handleChange}
-                  value={formData.phoneNumber}
-                  placeholder="Phone Number"
-                  required
-                />
-              </Form.Field>
-            </Grid.Column>
-            <Grid.Column>
-              <Form.Field>
-                <label>Occupation</label>
-                <input
-                  name="occupation"
-                  onChange={handleChange}
-                  value={formData.occupation}
-                  placeholder="Occupation"
-                  required
-                />
-              </Form.Field>
-              <Form.Field>
-                <label>Location</label>
-                <input
-                  name="location"
-                  onChange={handleChange}
-                  value={formData.location}
-                  placeholder="Location"
-                  required
-                />
-              </Form.Field>
-              <Form.Field>
-                <label>Account Type</label>
-                <Dropdown
-                  placeholder="Select account type"
-                  fluid
-                  selection
-                  options={accountOptions}
-                  onChange={handleDropdownChange}
-                  value={formData.accountType}
-                />
-              </Form.Field>
-            </Grid.Column>
-          </Grid>
-          <Form.Field>
-            <label>Next of Kin</label>
-            <input
-              name="nextOfKin"
-              onChange={handleChange}
-              value={formData.nextOfKin}
-              placeholder="Next of Kin"
-              required
-            />
-          </Form.Field>
-          <Form.Field>
-            <label>Next of Kin Phone Number</label>
-            <input
-              name="nextOfKinPhoneNumber"
-              onChange={handleChange}
-              value={formData.nextOfKinPhoneNumber}
-              placeholder="Next of Kin Phone Number"
-              required
-            />
-          </Form.Field>
-          {/* <Form.Button primary type="submit" onSubmit={handleSubmit} href="/">
-            Submit
-          </Form.Button> */}
-          <a href="/">
-            <button type="submit" className="btn btn-secondary submit">
-              Submit
-            </button>
-          </a>
-        </Form>
-      </Segment>
-    </Container>
+       <h1 className="signup-title" >Sign Up</h1>
+    <Segment>
+      <Form onSubmit={handleSubmit}>
+        <Grid columns={1} stackable>
+          <Grid.Column>
+            <Form.Field>
+              <label>Full Name</label>
+              <input
+                name="fullName"
+                onChange={handleChange}
+                value={formData.fullName}
+                placeholder="Full Name"
+                required
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Email</label>
+              <input
+                name="email"
+                onChange={handleChange}
+                value={formData.email}
+                placeholder="Email"
+                required
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Phone Number</label>
+              <input
+                name="phoneNumber"
+                onChange={handleChange}
+                value={formData.phoneNumber}
+                placeholder="Phone Number"
+                required
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Occupation</label>
+              <input
+                name="occupation"
+                onChange={handleChange}
+                value={formData.occupation}
+                placeholder="Occupation"
+                required
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Location</label>
+              <input
+                name="location"
+                onChange={handleChange}
+                value={formData.location}
+                placeholder="Location"
+                required
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Account Type</label>
+              <Dropdown
+                placeholder="Select account type"
+                fluid
+                selection
+                options={accountOptions}
+                onChange={handleDropdownChange}
+                value={formData.accountType}
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Next of Kin</label>
+              <input
+                name="nextOfKin"
+                onChange={handleChange}
+                value={formData.nextOfKin}
+                placeholder="Next of Kin"
+                required
+              />
+            </Form.Field>
+            <Form.Field>
+              <label>Next of Kin Phone Number</label>
+              <input
+                name="nextOfKinPhoneNumber"
+                onChange={handleChange}
+                value={formData.nextOfKinPhoneNumber}
+                placeholder="Next of Kin Phone Number"
+                required
+              />
+            </Form.Field>
+            <a href="/">
+              <button type="submit" className="btn btn-secondary submit">
+                Submit
+              </button>
+            </a>
+          </Grid.Column>
+        </Grid>
+      </Form>
+    </Segment>
+  </Container>
   );
 };
 
