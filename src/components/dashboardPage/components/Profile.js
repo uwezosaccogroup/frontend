3569896de79470794aa5750a618435bffa63e8b2
jos-the-top-dev <devs.josia@gmail.com>
2023-03-06 11:05:12 +0300

    updated profile path to '/users'

--- src/components/dashboardPage/components/Profile.js
+++ src/components/dashboardPage/components/Profile.js
@@ -1,12 +1,12 @@
-import { useEffect, useState } from 'react';
-import { Route, Routes } from 'react-router-dom';
-import React from 'react';
-import NavBar from './NavBar';
-import axios from 'axios';
-import Savings from './Savings';
-import { Card } from 'semantic-ui-react';
-import SavingsAccount from './SavingsAccount';
-import CurrentAccount from './CurrentAccount';
+import { useEffect, useState } from "react";
+import { Route, Routes } from "react-router-dom";
+import React from "react";
+import NavBar from "./NavBar";
+import axios from "axios";
+import Savings from "./Savings";
+import { Card } from "semantic-ui-react";
+import SavingsAccount from "./SavingsAccount";
+import CurrentAccount from "./CurrentAccount";
 
 const ProfileForm = () => {
   const [userData, setUserData] = useState({});
@@ -14,32 +14,39 @@ const ProfileForm = () => {
 
   useEffect(() => {
     axios
-      .get('https://uwezosacco.up.railway.app/')
+      .get("https://uwezosacco.up.railway.app/users")
       .then((response) => {
         const lastUser = response.data[response.data.length - 1];
         setUserData(lastUser);
-        setHasCurrentAccount(lastUser.account_type === 'current');
+        setHasCurrentAccount(lastUser.account_type === "current");
       })
       .catch((error) => {
-        console.error('Error fetching data: ', error);
+        console.error("Error fetching data: ", error);
       });
   }, []);
 
   return (
-    <div className='profile-container' style={{ backgroundColor: 'black', color: 'white' }}>
+    <div
+      className="profile-container"
+      style={{ backgroundColor: "black", color: "white" }}
+    >
       <Card.Group>
-        <Card style={{ backgroundColor: 'black', color: 'white' }}>
+        <Card style={{ backgroundColor: "black", color: "white" }}>
           <Card.Content>
-            <Card.Header style={{color: 'white'}}>{userData.name}</Card.Header>
-            <Card.Meta style={{color: 'white'}} >Email:{userData.email}</Card.Meta>
+            <Card.Header style={{ color: "white" }}>
+              {userData.name}
+            </Card.Header>
+            <Card.Meta style={{ color: "white" }}>
+              Email:{userData.email}
+            </Card.Meta>
             <br></br>
-            <Card.Description style={{ color: 'white' }}>
+            <Card.Description style={{ color: "white" }}>
               <p>Phone: {userData.phone}</p>
               <br></br>
               <p>Location: {userData.location}</p>
               <br></br>
               <p>Occupation: {userData.occupation}</p>
-               <br></br>
+              <br></br>
               <p>Account Type: {userData.account_type}</p>
               <br></br>
               <p>Next of Kin: {userData.next_of_kin}</p>
@@ -49,19 +56,23 @@ const ProfileForm = () => {
           </Card.Content>
         </Card>
         {hasCurrentAccount ? (
-          <Card style={{ backgroundColor: 'black', color: 'white' }}>
+          <Card style={{ backgroundColor: "black", color: "white" }}>
             <Card.Content>
-              <Card.Header style={{color: 'white'}} >Current Account</Card.Header>
-              <Card.Description style={{ color: 'white' }}>
+              <Card.Header style={{ color: "white" }}>
+                Current Account
+              </Card.Header>
+              <Card.Description style={{ color: "white" }}>
                 <CurrentAccount />
               </Card.Description>
             </Card.Content>
           </Card>
         ) : (
-          <Card style={{ backgroundColor: 'black', color: 'white' }}>
+          <Card style={{ backgroundColor: "black", color: "white" }}>
             <Card.Content>
-              <Card.Header style={{color: 'white'}} >Savings Account</Card.Header>
-              <Card.Description style={{ color: 'white' }}>
+              <Card.Header style={{ color: "white" }}>
+                Savings Account
+              </Card.Header>
+              <Card.Description style={{ color: "white" }}>
                 <SavingsAccount />
               </Card.Description>
             </Card.Content>
@@ -70,7 +81,6 @@ const ProfileForm = () => {
       </Card.Group>
     </div>
   );
-  
 };
 
 function Profile() {
@@ -78,10 +88,10 @@ function Profile() {
     <>
       <NavBar />
       <Routes>
-        <Route exact path='/' element={<ProfileForm />} />
+        <Route exact path="/" element={<ProfileForm />} />
         {/* <Route exact path='/withdraw' element={<WithdrawForm />} />
         <Route exact path='/deposit' element={<DepositForm />} /> */}
-        <Route exact path='/savings' element={<Savings />} />
+        <Route exact path="/savings" element={<Savings />} />
       </Routes>
     </>
   );
